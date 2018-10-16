9c18ac640d0a16301d7438fcc48aca27749dea07
peng <527453497@qq.com>
2018-10-16 23:12:53 +0800

    LinkedList

--- datastructure/src/linkedlist/LinkedList.java
+++ datastructure/src/linkedlist/LinkedList.java
@@ -23,4 +23,74 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public LinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * 获取链表中的元素个数
+     * @return
+     */
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * 返回链表是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    /**
+     * 在链表的头部添加新的元素
+     * @param e
+     */
+    public void addFirst(E e){
+        /*Node node = new Node(e);
+        node.next=head;
+        head=node;*/
+        head = new Node(e,head);
+        size ++;
+    }
+
+    /**
+     * 在链表的index（0-based）位置添加新的元素
+     * @param index
+     * @param e
+     */
+    public void add(int index,E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+
+            /*Node node = new Node(e);
+            node.next=prev.next;
+            prev.next=node;*/
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+    }
+
+    /**
+     * 在链表的末尾添加新的元素e
+     * @param e
+     */
+    public void addLast(E e){
+        add(size,e);
+    }
 }
