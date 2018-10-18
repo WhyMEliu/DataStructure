84934e75fcc41e504c03fb467e0f0cc3ab3fd08b
peng <527453497@qq.com>
2018-10-18 22:30:09 +0800

    日常更新~

--- datastructure/src/Main.java
+++ datastructure/src/Main.java
@@ -3,7 +3,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("~~~tech otaku save the world~~~");
-
-
     }
 }
--- datastructure/src/queue/TestMain.java
+++ datastructure/src/queue/TestMain.java
@@ -33,5 +33,8 @@ public class TestMain {
         double time2= testQueue(Lq,opCount);
         System.out.println("LoopQueue, time: " + time2 + " s ");
 
+        LinkedListQueue<Integer> LLq = new LinkedListQueue<>();
+        double time3= testQueue(LLq,opCount);
+        System.out.println("LinkedListQueue, time: " + time3 + " s ");
      }
 }
