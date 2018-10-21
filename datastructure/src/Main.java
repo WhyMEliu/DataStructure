1e7fa1abe000da21586f28c5b4bfda56672ea845
WhyMEliu <527453497@qq.com>
2018-10-21 13:04:46 +0800

    mtest

--- datastructure/src/Main.java
+++ datastructure/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("~~~tech otaku save the world~~~");
+        //System.out.println("~~~tech otaku save the world~~~");
+		System.out.println("~~~计算机计算机d~~~");
     }
 }
