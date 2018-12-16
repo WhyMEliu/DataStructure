b841b771de4d488acc5d8b49ca329e1131e85a03
WhyMEliu <527453497@qq.com>
2018-12-16 21:28:30 +0800

    clean project

---
+++ datastructure/src/segmentTree/Merger.java
@@ -0,0 +1,5 @@
+package segmentTree;
+
+public interface Merger<E> {
+	E Merger(E a, E b);
+}
--- datastructure/src/segmentTree/SegmentTree.java
+++ datastructure/src/segmentTree/SegmentTree.java
@@ -3,14 +3,41 @@ package segmentTree;
 public class SegmentTree<E> {
 	private E[] tree;
 	private E[] data;
+	private Merger<E> merger;
 	
-	public SegmentTree(E[] arr){
+	public SegmentTree(E[] arr, Merger<E> merger){
+		this.merger = merger;
 		data = (E[])new Object[arr.length];
 		for (int i = 0; i < arr.length; i++) {
 			data[i] = arr[i];
 		}
 		tree = (E[])new Object[4 * arr.length];
+		buildSegmentTree(0, 0, data.length - 1);
 	}
+	/**
+	 * 在treeIndex的位置创建表示区间[l...r]的线段树
+	 * @param treeIndex
+	 * @param l
+	 * @param r
+	 */
+	private void buildSegmentTree(int treeIndex, int l, int r){
+		
+		if(l == r){
+			tree[treeIndex] = data[l];
+			return;
+		}
+		
+		int leftTreeIndex = leftChild(treeIndex);
+		int rightTreeIndex = rightChild(treeIndex);
+		
+		int mid = l + (r - l) / 2;
+		
+		buildSegmentTree(leftTreeIndex, l, mid);
+		buildSegmentTree(rightTreeIndex, mid + 1, r);
+		
+		tree[treeIndex] = merger.Merger(tree[leftTreeIndex], tree[rightTreeIndex]);
+	}
+	
 	
 	public int getSize(){
 		return data.length;
@@ -37,6 +64,102 @@ public class SegmentTree<E> {
 	 * @return
 	 */
 	private int rightChild(int index){
-		return 2*index +1;
+		return 2*index +2;
 	}
+	
+	/**
+	 * 返回区间[query,queryr]的值
+	 * @param queryL
+	 * @param queryR
+	 * @return
+	 */
+	public E query (int queryL, int queryR){
+		if(queryL < 0 || queryL >= data.length ||
+				queryR < 0 || queryL > data.length || queryL > queryR){
+			throw new IllegalArgumentException("Index is illegal.");
+		}
+		return query(0, 0, data.length - 1, queryL, queryR);
+		
+	}
+	
+	/**
+	 * 在以treeIndex为根的线段树中[l...r]的范围里，搜索区间[queryL...queryR]的值
+	 * @param treeIndex
+	 * @param l
+	 * @param r
+	 * @param queryL
+	 * @param queryR
+	 * @return
+	 */
+	private E query(int treeIndex, int l, int r, int queryL, int queryR){
+		
+		if(l == queryL && r == queryR){
+			return tree[treeIndex];
+		}
+		
+		int mid = l + (r - l)/2;
+		
+		int leftTreeIndex = leftChild(treeIndex);
+		int rightTreeIndex = rightChild(treeIndex);
+		
+		if(queryL >= mid + 1){
+			return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+		}else if(queryR <= mid){
+			return query(leftTreeIndex, l, mid, queryL, queryR);
+		}
+		
+		E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+		E rightResult = query(rightTreeIndex, mid+1, r, mid+1, queryR);
+		
+		return merger.Merger(leftResult, rightResult);
+		
+	}
+	
+	public void set(int index, E e){
+		if(index < 0 || index >= data.length){
+			throw new IllegalArgumentException("Index is illegal");
+		}
+		
+		data[index] = e;
+		set(0, 0, data.length -1, index, e);
+	}
+	
+	
+	private void set(int treeIndex, int l, int r, int index, E e){
+		
+		if(l == r){
+			tree[treeIndex] = e;
+			return;
+		}
+		
+		int mid = l + (r - l)/2;
+		int leftTreeIndex = leftChild(treeIndex);
+		int rightTreeIndex = rightChild(treeIndex);
+		if(index >= mid + 1){
+			set(rightTreeIndex, mid + 1, r, index, e);
+		}else{
+			set(leftTreeIndex, l, mid, index, e);
+		}
+		
+		tree[treeIndex] = merger.Merger(tree[leftTreeIndex], tree[rightTreeIndex]);
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder res = new StringBuilder();
+		res.append('[');
+		for (int i = 0; i < tree.length; i++) {
+			if(tree[i] != null){
+				res.append(tree[i]);
+			}else{
+				res.append("null");
+			}
+			if(i != tree.length - 1){
+				res.append(", ");
+			}
+		}
+		res.append(']');
+		return res.toString();
+	}
+	
 }
